4f399451fd01e9c3af5ef31123190f90e445feb0
imshi187 <125292919+imshi187@users.noreply.github.com>
2023-06-27 19:05:55 +0800

    Update sliceutil.go

--- sliceutil.go
+++ sliceutil.go
@@ -5,12 +5,12 @@ import "fmt"
 // SliceUtil slice结构体
 type SliceUtil struct {
 	// interface{}代表任何类型
-	elements []interface{}
+	Elements []interface{}
 }
 
 // HasElement 是否有某个元素
 func (s SliceUtil) HasElement(element interface{}) bool {
-	for _, v := range s.elements {
+	for _, v := range s.Elements {
 		// 找到元素
 		if v == element {
 			return true
@@ -21,15 +21,15 @@ func (s SliceUtil) HasElement(element interface{}) bool {
 
 // Length 返回长度
 func (s SliceUtil) Length() int {
-	return len(s.elements)
+	return len(s.Elements)
 }
 
 // Add 增加元素
 func (s *SliceUtil) Append(element interface{}, args ...interface{}) SliceUtil {
 
-	s.elements = append(s.elements, element)
+	s.Elements = append(s.Elements, element)
 	if len(args) > 0 {
-		s.elements = append(s.elements, args...)
+		s.Elements = append(s.Elements, args...)
 	}
 	return (*s)
 }
@@ -37,29 +37,29 @@ func (s *SliceUtil) Append(element interface{}, args ...interface{}) SliceUtil {
 // Delete 按照元素本身来删除元素 (尚未成功)
 func (s *SliceUtil) Delete(i int) SliceUtil {
 
-	if s.elements == nil || len(s.elements) == 0 {
+	if s.Elements == nil || len(s.Elements) == 0 {
 		return (*s)
 	}
-	if i < 0 || i >= len(s.elements) {
+	if i < 0 || i >= len(s.Elements) {
 		panic("index out of bounds")
 	}
 
-	copy(s.elements[i:], s.elements[i+1:])
-	s.elements = s.elements[:len(s.elements)-1]
+	copy(s.Elements[i:], s.Elements[i+1:])
+	s.Elements = s.Elements[:len(s.Elements)-1]
 
 	return (*s)
 }
 
 // modify
 func (s *SliceUtil) Modify(index int, element interface{}) SliceUtil {
-	s.elements[index] = element
+	s.Elements[index] = element
 
 	return (*s)
 }
 
 // Where 查找到element所在的index
 func (s SliceUtil) Where(toFindElement interface{}) int {
-	for i, element := range s.elements {
+	for i, element := range s.Elements {
 		//找到所在元素
 		if element == toFindElement {
 			return i
@@ -71,12 +71,12 @@ func (s SliceUtil) Where(toFindElement interface{}) int {
 // 找到元素 by index
 // 索引由用户自己判断
 func (s SliceUtil) FindByIndex(index int) interface{} {
-	return s.elements[index]
+	return s.Elements[index]
 }
 
 // Foreach foreach成功实现
 func (s SliceUtil) Foreach(callback func(index int, item interface{})) {
-	for index, element := range s.elements {
+	for index, element := range s.Elements {
 		callback(index, element)
 	}
 }
@@ -86,12 +86,12 @@ func (s SliceUtil) Foreach(callback func(index int, item interface{})) {
 func (s SliceUtil) Filter(condition func(item interface{}) bool) SliceUtil {
 	// 这样可以chain invoke
 	var s1 SliceUtil
-	s1.elements = []interface{}{}
+	s1.Elements = []interface{}{}
 	s.Foreach(func(currentIndex int, eachElement interface{}) {
 		// 如果条件成立: 使用用户传入的回调函数作为判断条件
 		if condition(eachElement) {
 			// 添加元素
-			s1.elements = append(s1.elements, eachElement)
+			s1.Elements = append(s1.Elements, eachElement)
 		}
 	})
 	return s1
@@ -101,14 +101,14 @@ func (s SliceUtil) Filter(condition func(item interface{}) bool) SliceUtil {
 func (s SliceUtil) MapTo(callback func(item interface{}) interface{}) SliceUtil {
 	s.Foreach(func(currentIndex int, eachElement interface{}) {
 		// 对每一个元素就行重新赋值
-		s.elements[currentIndex] = callback(eachElement)
+		s.Elements[currentIndex] = callback(eachElement)
 	})
 	return s
 }
 
 // AllSatisfied 判断每个元素是否都满足条件
 func (s SliceUtil) AllMatch(judge func(item interface{}) bool) bool {
-	for _, element := range s.elements {
+	for _, element := range s.Elements {
 		// 如果有元素不满足条件，立刻返回
 		if !judge(element) {
 			return false
@@ -120,7 +120,7 @@ func (s SliceUtil) AllMatch(judge func(item interface{}) bool) bool {
 // -1表示找不到，否则返回index
 func (s *SliceUtil) findFirst(toFindElement interface{}) int {
 
-	for i, element := range s.elements {
+	for i, element := range s.Elements {
 		// 判断是否符合条件
 		if toFindElement == element {
 			return i
@@ -131,8 +131,8 @@ func (s *SliceUtil) findFirst(toFindElement interface{}) int {
 
 // -1表示找不到，否则返回index
 func (s *SliceUtil) findLast(element interface{}) int {
-	for i := len(s.elements) - 1; i >= 0; i-- {
-		if s.elements[i] == element {
+	for i := len(s.Elements) - 1; i >= 0; i-- {
+		if s.Elements[i] == element {
 			return i
 		}
 	}
@@ -153,22 +153,22 @@ func (s SliceUtil) TakeWhile(callback func(ele interface{}) bool) []interface{} 
 
 }
 
-// func main() {
-// 	var intS SliceUtil
-// 	// SliceUtil结构体的field仅仅包括[]interface{}类型的elements
-// 	intS.elements = []interface{}{1, 2, 3, 100, 200}
+func main() {
+	var intS SliceUtil
+	// SliceUtil结构体的field仅仅包括[]interface{}类型的elements
+	intS.Elements = []interface{}{1, 2, 3, 100, 200}
 
-// 	fmt.Println("------------------------------------")
-// 	fmt.Println(intS.TakeWhile(func(ele interface{}) bool {
-// 		// 一般都是同一类型的数据，这里是int类型
-// 		return ele.(int) > 2
-// 	}))
+	fmt.Println("------------------------------------")
+	fmt.Println(intS.TakeWhile(func(ele interface{}) bool {
+		// 一般都是同一类型的数据，这里是int类型
+		return ele.(int) > 2
+	}))
 
-// 	// filter会对s本身就行操作，takewhile方法只是返回满足条件的元素
-// 	intS.Filter(func(element interface{}) bool {
-// 		return element.(int) >= 100
-// 	}).Foreach(func(index int, item interface{}) {
-// 		fmt.Println(item)
-// 	})
+	// filter会对s本身就行操作，takewhile方法只是返回满足条件的元素
+	intS.Filter(func(element interface{}) bool {
+		return element.(int) >= 100
+	}).Foreach(func(index int, item interface{}) {
+		fmt.Println(item)
+	})
 
-// }
+}
